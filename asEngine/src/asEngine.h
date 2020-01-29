36a04ff246905e371085680ff9f768eb46b379bc
Wang <as5818956@gmail.com>
2020-01-29 23:48:33 +0900

    1

---
+++ asEngine/src/asEngine.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// NOTE:
+// The purpose of this file is to expose all engine features.
+// It should be included in the engine's implementing project not the engine itself!
+// It should be included in the precompiled header if available.
